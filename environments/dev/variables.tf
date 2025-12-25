e14f92f492bb264b7f7283e1c53d439a0c9d6616
Ubuntu <ubuntu@ip-172-31-33-128.ap-northeast-3.compute.internal>
2025-12-25 11:35:39 +0000

    added changes to the environment/variables.tf file

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,16 +1,16 @@
 variable "terraform-backend-bucket-name"{
     description = "This is for the name of the backend bucket"
-    type = "string"
+    type = string
     default = ""
 }
 variable "terrafrom-backend-key"{
     description = "this is for the terraform backend key"
-    type = "string"
+    type = string
     default = "dev/terraform.tfstate"
 }
 variable "terraform-default-region"{
     description = "It is going to be the default region for every resource created"
-    type = "string"
+    type = string
     default = "ap-northeast-3"
 }
 
