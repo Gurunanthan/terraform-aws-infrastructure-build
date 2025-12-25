09adf41d4ae49f38aaeaa8161054746b40e5a9ff
Ubuntu <ubuntu@ip-172-31-33-128.ap-northeast-3.compute.internal>
2025-12-25 12:07:43 +0000

    added modularity and made changes to the vpc/output.tf and variables.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,11 +4,4 @@ provider "aws" {
 
 module "vpc" {
   source = "../../modules/vpc"
-
-  vpc_cidr = "10.0.0.0/16"
-
-  tags = {
-    Project = "terraform-aws-infrastructure-build"
-    Env     = "dev"
-  }
 }
